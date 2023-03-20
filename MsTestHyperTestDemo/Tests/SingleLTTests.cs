5c2594ba2cac4d07a4ca52a1c735d183bc3492db
Meera <pmeeraa@gmail.com>
2023-03-20 11:33:44 +0530

    source code

--- MsTestHyperTestDemo/Tests/SingleLTTests.cs
+++ MsTestHyperTestDemo/Tests/SingleLTTests.cs
@@ -6,7 +6,7 @@ using System.Threading;
 
 namespace SingleLTTests
 {
-    [TestClass("chrome", "latest", "Windows 10")]
+        [TestClass]
         [TestCategory("ToDoTest")]
         public class MsTestSeleniumSample
         {
@@ -14,70 +14,76 @@ namespace SingleLTTests
             public static string LT_ACCESS_KEY = Environment.GetEnvironmentVariable("LT_ACCESS_KEY") == null ? "LT_ACCESS_KEY" : Environment.GetEnvironmentVariable("LT_ACCESS_KEY");
             public static string gridURL = "@hub.lambdatest.com/wd/hub";
 
-            ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
+          //  ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
+             IWebDriver driver;
             public String browser;
             public String version;
             public String os;
+            DesiredCapabilities capabilities = new DesiredCapabilities();
 
             [TestInitialize]
             public void Init()
             {
-                DesiredCapabilities capabilities = new DesiredCapabilities();
-                capabilities.SetCapability(CapabilityType.BrowserName, browser);
-                capabilities.SetCapability(CapabilityType.Version, version);
-                capabilities.SetCapability(CapabilityType.Platform, os);
-                capabilities.SetCapability("build", "[HyperTest] Selenium C# ToDo Demo1");
+                capabilities.SetCapability("video",true);
+                capabilities.SetCapability("visual",true);
+                capabilities.SetCapability("network",true);
+                capabilities.SetCapability("console",true);
+                capabilities.SetCapability("terminal",true);
 
+            }
+
+
+            [DataTestMethod]
+            [DataRow("chrome", "72.0", "Windows 10")]
+            [TestMethod]
+
+            public void Todotest(String browser, String version, String os)
+            {
+                String itemName = "Yey, Let's add it to list";
+
+                capabilities.SetCapability("browserName", browser);
+                capabilities.SetCapability("version", version);
+                capabilities.SetCapability("platform", os);
                 capabilities.SetCapability("user", LT_USERNAME);
                 capabilities.SetCapability("accessKey", LT_ACCESS_KEY);
+                capabilities.SetCapability("build", "LT ToDoApp using MsTest on LambdaTest");
+                capabilities.SetCapability("name", "LT ToDoApp using MsTest on LambdaTest");
 
-                capabilities.SetCapability("video",true);
-                  capabilities.SetCapability("visual",true);
-                  capabilities.SetCapability("network",true);
-                capabilities.SetCapability("console",true);
-                      capabilities.SetCapability("terminal",true);
+                driver = new RemoteWebDriver(new Uri("https://" + LT_USERNAME + ":" + LT_ACCESS_KEY + gridURL), capabilities, TimeSpan.FromSeconds(2000));
+                driver.Url = "https://lambdatest.github.io/sample-todo-app/";
+                Assert.AreEqual("Sample page - lambdatest.com", driver.Title);
+                // Click on First Check box
+                IWebElement firstCheckBox = driver.FindElement(By.Name("li1"));
+                firstCheckBox.Click();
 
-driver = new RemoteWebDriver(new Uri("https://" + LT_USERNAME + ":" + LT_ACCESS_KEY + gridURL), capabilities, TimeSpan.FromSeconds(2000));
-                Console.Out.WriteLine(driver);
+                // Click on Second Check box
+                IWebElement secondCheckBox = driver.FindElement(By.Name("li2"));
+                secondCheckBox.Click();
+
+                // Enter Item name
+                IWebElement textfield = driver.FindElement(By.Id("sampletodotext"));
+                textfield.SendKeys(itemName);
+
+                // Click on Add button
+                IWebElement addButton = driver.FindElement(By.Id("addbutton"));
+                addButton.Click();
+
+                // Verified Added Item name
+                IWebElement itemtext = driver.FindElement(By.XPath("/html/body/div/div/div/ul/li[6]/span"));
+                String getText = itemtext.Text;
+                Assert.IsTrue(itemName.Contains(getText));
+
+                /* Perform wait to check the output */
+                System.Threading.Thread.Sleep(2000);
+
+                Console.WriteLine("LT_ToDo_Test Passed");
             }
 
-
-
-            [TestMethod]
-            public void Todotest()
+            [TestCleanup]
+            public void Cleanup()
             {
-                String context_name = TestContext.CurrentContext.Test.Name + " on " + browser + " " + version + " " + os;
-               // TC_Name = context_name;
-
-                //_test = _extent.CreateTest(context_name);
-
-                Console.WriteLine("Navigating to todos app.");
-                driver.Value.Navigate().GoToUrl("https://lambdatest.github.io/sample-todo-app/");
-
-                driver.Value.FindElement(By.Name("li4")).Click();
-                Console.WriteLine("Clicking Checkbox");
-                driver.Value.FindElement(By.Name("li5")).Click();
-
-                /* If both clicks worked, then te following List should have length 2 */
-                IList<IWebElement> elems = driver.Value.FindElements(By.ClassName("done-true"));
-
-                /* so we'll assert that this is correct. */
-                Assert.AreEqual(2, elems.Count);
-
-                Console.WriteLine("Entering Text");
-                driver.Value.FindElement(By.Id("sampletodotext")).SendKeys("Yey, Let's add it to list");
-                driver.Value.FindElement(By.Id("addbutton")).Click();
-
-                /* lets also assert that the new todo we added is in the list */
-                string spanText = driver.Value.FindElement(By.XPath("/html/body/div/div/div/ul/li[6]/span")).Text;
-                Assert.AreEqual("Yey, Let's add it to list", spanText);
-            }
-
-            [ClassCleanup]
-            protected void ExtentClose()
-            {
-                Console.WriteLine("OneTimeTearDown");
-                //_extent.Flush();
-            }
+                if (driver != null)
+                    driver.Quit();
+             }
         }
 }
